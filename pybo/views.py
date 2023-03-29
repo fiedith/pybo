ce489d269a6879ffe52d5bdd22f07f60e533a353
= <brianyhs@naver.com>
2023-03-30 01:13:46 +0900

    feat: added pages bar

--- pybo/views.py
+++ pybo/views.py
@@ -3,12 +3,16 @@ from django.utils import timezone
 from django.http import HttpResponseNotAllowed
 from .models import Question, Answer
 from .forms import QuestionForm, AnswerForm
+from django.core.paginator import Paginator
 
 # Create your views here.
 
 def index(request):
+    page = request.GET.get('page', '1')  # 페이지. # GET방식으로 호출된 url에서 page값을 가져올 때 사용됨
     question_list = Question.objects.order_by('-create_date')
-    context = {'question_list': question_list}
+    paginator = Paginator(question_list, 10)  # 페이지당 10개씩 보여주기
+    page_obj = paginator.get_page(page)
+    context = {'question_list': page_obj}   # question_list는 페이징 객체(page_obj)
     return render(request, 'pybo/question_list.html', context)
     # render: python data를 template에 적용하여 HTMl로 반환해주는 함수
 
