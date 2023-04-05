4451696c124953fddb922c8efef1a0bd59bad5d9
= <brianyhs@naver.com>
2023-04-05 14:55:36 +0900

    feat: added question edit feature

--- pybo/views.py
+++ pybo/views.py
@@ -4,6 +4,7 @@ from .models import Question, Answer
 from .forms import QuestionForm, AnswerForm
 from django.core.paginator import Paginator
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 # Create your views here.
 
@@ -52,5 +53,24 @@ def question_create(request):
             return redirect('pybo:index')
     else:   # else이므로 GET: "질문 등록하기" 버튼 -> 질문을 등록하기 위한 페이지를 띄워야 하므로
         form = QuestionForm()
+    context = {'form': form}
+    return render(request, 'pybo/question_form.html', context)
+
+@login_required(login_url='common:login')
+def question_modify(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    if request.user != question.author:         # 현재 login된 사용자와 질문의 글쓴이가 다를때
+        messages.error(request, '수정권한이 없습니다')
+        return redirect('pybo:detail', question_id=question.id)
+    if request.method == "POST":
+        form = QuestionForm(request.POST, instance=question)    # POST
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.modify_date = timezone.now()  # modify date 저장
+            question.save()
+            return redirect('pybo:detail', question_id=question.id)
+    else:
+        # instance를 기준으로 QuestionForm을 생성하지만 request.POST의 값으로 overwrite함
+        form = QuestionForm(instance=question)  # GET
     context = {'form': form}
     return render(request, 'pybo/question_form.html', context)
